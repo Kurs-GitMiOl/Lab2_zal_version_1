de9d98ba06d1c079eb4ce1336234758b93ff8e2f
Kurs GitMiOl <mirek_olszowka_git@o2.pl>
2025-11-03 19:15:20 +0100

    Lab2_zal calculator + testy klasy (tylko testy klasy wymagane w zadaniu)

--- test_calculator.py
+++ test_calculator.py
@@ -2,27 +2,73 @@ from calculator import Calculator
 import runpy
 
 # Test addition
-def test_sum():
+def test_sum_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.sum() == 15.0
 
+def test_sum_negative_numbers():
+    calc_1 = Calculator(-10.0, -5.0)
+    assert calc_1.sum() == -15.0
+
+def test_sum_mixed_sign_numbers():
+    calc_1 = Calculator(-10.0, 5.0)
+    assert calc_1.sum() == -5.0
+
+def test_sum_with_zero():
+    calc_1 = Calculator(10.0, 0.0)
+    assert calc_1.sum() == 10.0
 
 # Test subtraction
-def test_subtract():
+def test_subtract_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.subtract() == 5
 
+def test_subtract_negative_numbers():
+    calc_1 = Calculator(-5.5, -1.0)
+    assert calc_1.subtract() == -4.5
+
+def test_subtract_mixed_sign_numbers():
+    calc_1 = Calculator(-5.5, 1.0)
+    assert calc_1.subtract() == -6.5
+
+def test_subtract_with_zero():
+    calc_1 = Calculator(7.0, 0.0)
+    assert calc_1.subtract() == 7.0
+
 
 # Test multiplication
-def test_multiply():
+def test_multiply_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.multiply() == 50.0
 
 
+def test_multiply_negative_numbers():
+    calc_1 = Calculator(-10.0, -5.0)
+    assert calc_1.multiply() == 50.0
+
+def test_multiply_mixed_sign_numbers():
+    calc_1 = Calculator(-10.0, 5.0)
+    assert calc_1.multiply() == -50.0
+
+
+def test_multiply_with_zero():
+    calc_1 = Calculator(0.0, 5.0)
+    assert calc_1.multiply() == 0.0
+
+
 # Test division with correct numbers
-def test_division():
+def test_division_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.divide() == 2.0
+
+
+def test_division_negative_numbers():
+    calc_1 = Calculator(-10.0, -5.0)
+    assert calc_1.divide() == 2.0
+
+def test_division_mixed_sign_numbers():
+    calc_1 = Calculator(-10.0, 5.0)
+    assert calc_1.divide() == -2.0
 
 
 # Test division (for first implementation of division) with op2 == 0
